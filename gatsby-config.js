1e1d9d3bb1cc165455f292155506ecbd058b14d2
Sam Wellander <samwellander@gmail.com>
2018-11-09 19:50:29 -0500

    add page creation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [`gatsby-plugin-netlify-cms`, `gatsby-transformer-remark`, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      path: `${__dirname}/gigs`,
+      name: "markdown-pages",
+    }
+  }],
 }
